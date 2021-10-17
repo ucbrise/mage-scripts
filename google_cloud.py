4a0ce6905a5e5c4f23877c4b0f3136a4f37a280c
Sam Kumar <samkumar99@gmail.com>
2021-10-17 12:30:55 -0700

    Remove extraneous serviceAccounts field from Google Cloud JSON

--- google_cloud.py
+++ google_cloud.py
@@ -153,19 +153,6 @@ def spawn_instance(m, name, instance_type, num_local_ssds, image_name, disk_layo
         "reservationAffinity": {
             "consumeReservationType": "ANY_RESERVATION",
         },
-        "serviceAccounts": [
-            {
-                "email": "832515717505-compute@developer.gserviceaccount.com",
-                "scopes": [
-                    "https://www.googleapis.com/auth/devstorage.read_only",
-                    "https://www.googleapis.com/auth/logging.write",
-                    "https://www.googleapis.com/auth/monitoring.write",
-                    "https://www.googleapis.com/auth/servicecontrol",
-                    "https://www.googleapis.com/auth/service.management.readonly",
-                    "https://www.googleapis.com/auth/trace.append"
-                ]
-            }
-        ],
         "shieldedInstanceConfig": {
             "enableSecureBoot": False,
             "enableVtpm": True,
